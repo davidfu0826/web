0d2460032a57e96bb6ad11d2150953a55823fafc
Per-Victor Persson <dracyr@gmail.com>
2014-07-14 15:27:10 +0200

    Devise views and flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+      when "notice" then "alert alert-info alert-dismissable"
+      when "success" then "alert alert-success alert-dismissable"
+      when "error" then "alert alert-danger"
+      when "alert" then "alert alert-warning"
+    end
+  end
 end
