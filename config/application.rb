c78e60a7e9f9c4a8e8872adb16b7a7ad447fbdde
Per-Victor Persson <dracyr@gmail.com>
2014-07-14 13:28:37 +0200

    Gem Setup

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module Tlth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.controller_specs false
+      g.view_specs false
+      g.helper_specs false
+      g.model_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
