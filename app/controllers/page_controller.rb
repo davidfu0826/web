5372d2b4af8e1d46260827236abaf45a8fef75cb
Per-Victor Persson <dracyr@gmail.com>
2014-07-14 14:31:05 +0200

    Posts

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,5 @@
 class PageController < ApplicationController
   def index
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   root 'page#index'
 
   devise_for :users
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
+  resources :posts
 end
