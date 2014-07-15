7357c7977c3039e43d9163eefa6eff8117632f69
Per-Victor Persson <dracyr@gmail.com>
2014-07-15 14:42:22 +0200

    Improved navigation handling, fixes #7

--- app/controllers/nav_items_controller.rb
+++ app/controllers/nav_items_controller.rb
@@ -1,22 +1,35 @@
 class NavItemsController < ApplicationController
 
+  def index
+    @nav_items = NavItem.orphans
+  end
+
   def new
     @nav_item = NavItem.new
     @nav_items = NavItem.all
     @pages = Page.all
+    @parent = NavItem.find(params[:parent])
   end
 
   def create
     @nav_item = NavItem.new(nav_item_params)
-
     if @nav_item.save
-      redirect_to root_path
+      redirect_to nav_items_path
     else
+      @parent = NavItem.find(nav_item_params[:parent_id])
+      @pages = Page.all
       render 'new'
     end
   end
 
   def edit
+  end
+
+  def destroy
+    nav_item = NavItem.find(params[:id])
+    nav_item.destroy
+
+    redirect_to nav_items_path
   end
 
   private
--- app/models/nav_item.rb
+++ app/models/nav_item.rb
@@ -1,9 +1,27 @@
 class NavItem < ActiveRecord::Base
   belongs_to :page
   belongs_to :parent, class_name: "NavItem"
-  has_many :children, class_name: "NavItem", foreign_key: "parent_id"
+  has_many :children, class_name: "NavItem", foreign_key: "parent_id", dependent: :destroy
+
+  scope :orphans, -> { where(parent: nil) }
+
+  validate :has_title_or_page
 
   def title
-    page.title || read_attribute(:title)
+    if page
+      page.title
+    else
+      read_attribute(:title)
+    end
+  end
+
+  def has_page?
+    page ? true : false
+  end
+
+  def has_title_or_page
+    if parent.present? ^ title.present? #XOR
+      errors.add(:title, "Måste tilldelas en sida eller en titel")
+    end
   end
 end
