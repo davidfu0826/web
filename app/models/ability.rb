2e4fb63989994972103126f43123a131f82916fe
Per-Victor Persson <dracyr@gmail.com>
2014-08-05 10:06:32 +0200

    Hide panels for unauthorized users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
+      can [:index, :archive], Post
     end
     #
     # The first argument to `can` is the action you are giving the user
