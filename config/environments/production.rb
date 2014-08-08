fa6ddf6f811fde16cac6c9c19e141d20774149ad
Per-Victor Persson <dracyr@gmail.com>
2014-08-08 13:49:32 +0200

    Fix event link, fixes #49

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  PUBLIC_URL= "tlth.se/"
+  PUBLIC_URL= "http://www.tlth.se/"
 end
