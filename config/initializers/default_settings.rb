a15895701ce64e6d032fa7082e64b1b15a0c01b6
Per-Victor Persson <dracyr@gmail.com>
2014-08-22 13:30:09 +0200

    Fix misspelling

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,1 +1,1 @@
-Setting.defaults['sidebar_links'] = {"Facebook"=>"https://www.facebook.com/pages/Teknologk%C3%A5ren-vid-LTH/15808557477", "KFS AB"=>"http://www.kfsab.se/"}
+Settings.defaults['sidebar_links'] = {"Facebook"=>"https://www.facebook.com/pages/Teknologk%C3%A5ren-vid-LTH/15808557477", "KFS AB"=>"http://www.kfsab.se/"}
