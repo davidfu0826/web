1b929e0e95a0d7ae9ed4a009df1480ae57e79a89
Per-Victor Persson <dracyr@gmail.com>
2015-01-27 15:45:11 +0100

    Cleaned up events#index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -100,9 +100,9 @@ class EventsController < ApplicationController
   end
 
   def events_by_month_and_week(events)
-    events = events.group_by { |u| u.start_time.to_time.beginning_of_month }
-    events.each_key do |k|
-      events[k] = events[k].group_by { |e| e.start_time.strftime("%V").to_i }
+    events = events.group_by { |u| u.start_time.beginning_of_month }
+    events.each_key do |month|
+      events[month] = events[month].group_by { |e| e.start_time.strftime("%V").to_i }
     end
     events
   end
