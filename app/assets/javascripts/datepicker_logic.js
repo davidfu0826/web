b541e464a0f7884b491658c0362c252b7bc1bf8b
Richard Luong <richardluong92@gmail.com>
2017-03-30 19:58:04 +0200

    Add logic to datepicker

---
+++ app/assets/javascripts/datepicker_logic.js
@@ -0,0 +1,21 @@
+$(document).on("turbolinks:load", function() {
+
+	$('#event_start_time').datetimepicker();
+	$('#event_end_time').datetimepicker({
+            useCurrent: false //Important! See issue #1075
+        });
+	$("#event_start_time").on("dp.change", function (e) {
+		$('#event_end_time').data("DateTimePicker").minDate(e.date);
+
+		// if end_time is empty
+		if ($('#event_end_time').data("DateTimePicker").date() == null) {
+			$('#event_end_time').data("DateTimePicker").date($("#event_start_time").data("DateTimePicker").date().add(3, 'hours'));
+		} else {
+			interval = $('#event_end_time').data("DateTimePicker").date()-e.oldDate;
+			new_date = $('#event_start_time').data("DateTimePicker").date().add(interval, 'ms');
+			$('#event_end_time').data("DateTimePicker").date(new_date);
+		}
+
+	});
+
+});
