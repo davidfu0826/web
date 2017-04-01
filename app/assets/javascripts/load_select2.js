ef942de640b42da8f3a8282dde2c7801cbc1c33e
David Wessman <david.wesmn@gmail.com>
2017-04-01 18:37:53 +0200

    select2 loading, close flash
    - Fixes bad space in tag-factory
    - Adds some locales
    - Refactor style in image.rb

--- app/assets/javascripts/load_select2.js
+++ app/assets/javascripts/load_select2.js
@@ -1,20 +1,26 @@
 function select2() {
   $('.select2').select2({
+    debug: true,
     theme: 'bootstrap',
-    width: '100%',
-    debug: true
+    width: '100%'
   });
 }
 
 function select2Tags() {
   $('.select2_tags').select2({
+    debug: true,
     tags: true,
     theme: 'bootstrap',
-    width: '100%',
-    debug: true
+    width: '100%'
   });
 }
 
-$(select2);
-$(select2Tags);
+// Clear all select2 insertions before caching to avoid double loading.
+function clearSelect2() {
+  $('.select2').select2('destroy');
+  $('.select2_tags').select2('destroy');
+}
 
+$(document).on('turbolinks:load', select2);
+$(document).on('turbolinks:load', select2Tags);
+$(document).on('turbolinks:before_cache', clearSelect2);
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,13 +3,14 @@ class Image < ActiveRecord::Base
   include Tagable
 
   before_validation(on: [:create, :update]) do
-    taggings.each do |t|
-      t.taggable = self
-    end
+    taggings.each { |t| t.taggable = self }
   end
   validates :image, presence: true
-  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp, :gif], case_sensitive: false,
-                   message: I18n.t('errors.messages.image_format'), if: :image_changed?
+  validates_property(:format, of: :image,
+                     in: [:jpeg, :jpg, :png, :bmp, :gif],
+                     case_sensitive: false,
+                     message: I18n.t('errors.messages.image_format'),
+                     if: :image_changed?)
 
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
@@ -17,11 +18,13 @@ class Image < ActiveRecord::Base
   has_many :posts
   has_many :pages
 
-  scope :tag,    -> (tag_id) { joins(:tags).where( 'tags.id' => tag_id ) }
-  scope :search, -> (search) {
+  scope :tag, ->(tag_id) { joins(:tags).where(tags: { id: tag_id }) }
+  scope :search, ->(search) {
     image_ids =  find_by_fuzzy_image_name(search).map(&:id)
     image_ids << find_by_fuzzy_title(search).map(&:id)
-    image_ids << joins(:tags).where( "lower(tags.title) LIKE :search_param", search_param: search.downcase).map(&:id)
+    image_ids << joins(:tags).where("lower(tags.title) LIKE :search_param",
+                                    search_param: search.downcase)
+                 .map(&:id)
     where(id: image_ids.flatten)
   }
 
