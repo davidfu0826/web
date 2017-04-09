090328567360faff482a33ebd762c5e434c64286
David Wessman <david.wesmn@gmail.com>
2017-04-10 00:22:12 +0200

    Images: fixes tags and searching
    - Clean up unused code

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,8 +3,9 @@ class ImagesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @images = @images.filter(filtering_params)
-                     .page(params[:page]).per(20)
+    @images = @images.filter(search_params)
+                     .page(params[:page])
+                     .per(20)
   end
 
   def new; end
@@ -15,11 +16,14 @@ class ImagesController < ApplicationController
     respond_to do |format|
       if @image.save && @image.update(tag_ids: image_params.fetch(:tag_ids, []))
         format.html { redirect_to images_path, notice: t('.success') }
-        format.js { render action: 'upload_success' }
+        format.js   { render action: 'upload_success' }
       else
         load_tags
-        format.html { render :new }
-        format.js { render json: @image.errors, status: :unprocessable_entity }
+        format.html { render :new, status: 422 }
+        format.js do
+          render json: @image.errors,
+                 status: :unprocessable_entity
+        end
       end
     end
   end
@@ -28,39 +32,34 @@ class ImagesController < ApplicationController
 
   def update
     if @image.update(image_params)
-      redirect_to images_path, notice: t('.success')
+      redirect_to edit_image_path(@image), notice: 'Image updated'
     else
       load_tags
-      render 'edit'
+      render :edit, status: 422
     end
   end
 
   def destroy
     @image.destroy!
-
     redirect_to images_path
   end
 
   # Used by image modal
   def search
-    @images = @images.filter(filtering_params)
+    @images = @images.filter(search_params)
   end
 
   private
 
   def image_params
-    params.require(:image).permit(:image, :title)
+    params.require(:image).permit(:image, :title, tag_ids: [])
   end
 
-  def image_tag_params
-    params.require(:image).permit(:tags)
-  end
-
-  def filtering_params
-    params.slice(:search, :tag)
+  def search_params
+    params.slice(:search, :tags)
   end
 
   def load_tags
-    @tags = Tag.all
+    @tags = Tag.order(:title)
   end
 end
--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe ImagesController, type: :controller do
   allow_user_to(:manage, Image)
 
-  describe 'GET #new' do
-    it 'sets new upload and renders' do
-      get(:new)
-
+  describe "GET#new" do
+    it "renders properly" do
+      get :new
+      expect(response).to render_template :new
       expect(response).to have_http_status(200)
       expect(assigns(:image)).to be_a_new(Image)
     end
@@ -24,27 +24,74 @@ RSpec.describe ImagesController, type: :controller do
     end
   end
 
+  describe "GET edit" do
+    it "render edit " do
+      image = create(:image)
+      get :edit, id: image.to_param
+      expect(response).to render_template :edit
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe 'POST #create' do
-    it 'valid parameters' do
-      attributes = { image: test_file, tags: [] }
+    it "valid params" do
+      tag = create(:tag)
+      attributes = {
+        title: 'Svensk',
+        tag_ids: [tag.id],
+        image: test_file
+      }
 
       expect do
-        post(:create, image: attributes)
+        post :create, image: attributes
       end.to change(Image, :count).by(1)
 
+      created = Image.last
       expect(response).to redirect_to(images_path)
+      expect(response).to have_http_status(302)
+      expect(created.tags.map(&:id)).to include(tag.id)
       expect(flash[:notice]).to eq(I18n.t('images.create.success'))
     end
 
     it 'invalid parameters' do
-      attributes = { image: nil, tags: [] }
+      attributes = { image: nil }
 
       expect do
         post(:create, image: attributes)
       end.to change(Image, :count).by(0)
 
       expect(response).to render_template(:new)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe "patch#update" do
+    it "valid params" do
+      tag = create(:tag)
+      resource = create(:image, title: "Gammal")
+
+      attributes = {
+        title: "Ny",
+        tag_ids: [tag.id]
+      }
+
+      patch :update, id: resource.id, image: attributes
+      resource.reload
+      expect(response).to redirect_to(edit_image_path(resource))
+      expect(response).to have_http_status(302)
+      expect(resource.title).to eq("Ny")
+      expect(resource.tags.map(&:id)).to include(tag.id)
+    end
+
+    it "invalid params" do
+      resource = create(:image, title: "Gammal")
+      attributes = { image: nil }
+
+      patch :update, id: resource.id, image: attributes
+      resource.reload
+      expect(response).to render_template(:edit)
+      expect(response).to have_http_status(422)
+      expect(resource.title).to eq("Gammal")
     end
   end
 
