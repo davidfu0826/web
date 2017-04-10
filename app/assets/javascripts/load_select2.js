f5582a9b40f69b29d73610c6e13c572735419707
David Wessman <david.wesmn@gmail.com>
2017-04-10 10:02:01 +0200

    Clean up for tags, some icons and some js

--- app/assets/javascripts/load_select2.js
+++ app/assets/javascripts/load_select2.js
@@ -1,6 +1,5 @@
 function select2() {
   $('.select2').select2({
-    debug: true,
     theme: 'bootstrap',
     width: '100%'
   });
@@ -8,7 +7,6 @@ function select2() {
 
 function select2Tags() {
   $('.select2_tags').select2({
-    debug: true,
     tags: true,
     theme: 'bootstrap',
     width: '100%'
@@ -17,10 +15,14 @@ function select2Tags() {
 
 // Clear all select2 insertions before caching to avoid double loading.
 function clearSelect2() {
-  $('.select2').select2('destroy');
-  $('.select2_tags').select2('destroy');
+  return $('.form-control.select2').each(function() {
+    return $(this).select2('destroy');
+  });
+  return $('.form-control.select2_tags').each(function() {
+    return $(this).select2('destroy');
+  });
 }
 
 $(document).on('turbolinks:load', select2);
 $(document).on('turbolinks:load', select2Tags);
-$(document).on('turbolinks:before_cache', clearSelect2);
+$(document).on('turbolinks:before-cache', clearSelect2);
--- app/models/concerns/fuzzy_search_titles.rb
+++ app/models/concerns/fuzzy_search_titles.rb
@@ -2,15 +2,15 @@ module FuzzySearchTitles
   extend ActiveSupport::Concern
 
   included do
-    scope :search, -> (search) {
+    scope :search, (lambda do |search|
       if I18n.locale == :sv
-        ids =  find_by_fuzzy_title_sv(search).map(&:id)
+        ids = find_by_fuzzy_title_sv(search).map(&:id)
       else
-        ids =  find_by_fuzzy_title_en(search).map(&:id)
+        ids = find_by_fuzzy_title_en(search).map(&:id)
       end
-      ids << joins(:tags).where(
-        "lower(tags.title) LIKE :search_param", search_param: search.downcase).map(&:id)
+      ids << joins(:tags).where("lower(tags.title) LIKE :search_param",
+                                search_param: search.downcase).pluck(:id)
       where(id: ids.flatten)
-    }
+    end)
   end
 end
