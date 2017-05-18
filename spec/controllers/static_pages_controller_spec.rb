b780f25c2f563f35fd06c27ef7ef6e3bc26e3950
Simon <simonfinnman@hotmail.com>
2017-05-18 18:15:37 +0200

    Adds static pages ability to spec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe(StaticPagesController, type: :controller) do
+  allow_user_to :manage, :static_pages
+
   describe('GET # robots') do
     it('renders robots as txt') do
       get(:robots, format: :txt)
