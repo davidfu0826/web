d14ea8ec949ad61f2f219e9acfbff4db396751e9
David Wessman <david.wesmn@gmail.com>
2017-06-20 15:19:09 +0200

    Images: remove old dragonfly field
    - Renames title method -> to_s

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620200759) do
+ActiveRecord::Schema.define(version: 20170620200760) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,6 @@ ActiveRecord::Schema.define(version: 20170620200759) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.string   "image_uid"
-    t.string   "image_name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
