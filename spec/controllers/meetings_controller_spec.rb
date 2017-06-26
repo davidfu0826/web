f8a55548406aa4a57282c227ef0761a0063e0b36
David Wessman <david.wesmn@gmail.com>
2017-06-26 10:59:14 +0200

    MeetingDocuments: allow anyone to view
    - Changes authorization in meeting_documents_controller
    - Adds a request spec and replaces parts of controller specs

--- spec/controllers/meetings_controller_spec.rb
+++ spec/controllers/meetings_controller_spec.rb
@@ -19,46 +19,6 @@ RSpec.describe(MeetingsController) do
     end
   end
 
-  describe('GET #new') do
-    it('renders properly') do
-      get(:new)
-      expect(response).to have_http_status(200)
-      expect(assigns(:meeting)).to be_a_new(Meeting)
-    end
-  end
-
-  describe('GET #edit') do
-    it('renders the edit meeting page') do
-      meeting = create(:meeting)
-      get(:edit, params: { id: meeting.to_param })
-      expect(response).to have_http_status(200)
-      expect(assigns(:meeting)).to eq(meeting)
-    end
-  end
-
-  describe('POST #create') do
-    it('valid parameters') do
-      attributes = { title: 'S18', year: '2017', kind: 'board', ranking: 0 }
-
-      expect do
-        post(:create, params: { meeting: attributes })
-      end.to change(Meeting, :count).by(1)
-
-      expect(response).to redirect_to(edit_meeting_path(Meeting.last))
-    end
-
-    it('invalid parameters') do
-      attributes = { title: 'No year!' }
-
-      expect do
-        post(:create, params: { meeting: attributes })
-      end.to change(Meeting, :count).by(0)
-
-      expect(response).to have_http_status(422)
-      expect(response).to render_template(:new)
-    end
-  end
-
   describe('PATCH #update') do
     it('valid parameters') do
       meeting = create(:meeting, title: 'S5')
@@ -81,29 +41,5 @@ RSpec.describe(MeetingsController) do
       meeting.reload
       expect(meeting.title).to eq('S9')
     end
-
-    it('creates document') do
-      meeting = create(:meeting)
-      attributes = { document_kind: :minute, file_sv: test_file }
-
-      expect do
-        patch(:update, params: { id: meeting.to_param, meeting: attributes })
-      end.to change(MeetingDocument, :count).by(1)
-    end
-  end
-
-  describe('DELETE # destroy') do
-    it 'destroys meeting' do
-      meeting = create(:meeting)
-
-      expect do
-        delete(:destroy, params: { id: meeting.to_param })
-      end.to change(Meeting, :count).by(-1)
-      expect(response).to redirect_to(meetings_path)
-    end
-  end
-
-  def test_file
-    Rack::Test::UploadedFile.new(File.open('spec/support/file.pdf'))
   end
 end
