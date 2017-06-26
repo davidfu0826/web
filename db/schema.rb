1960a16d07abc78d394a836bb2d98855a8871481
David Wessman <david.wesmn@gmail.com>
2017-06-26 13:37:06 +0200

    Dragonfly: be gone pest!
    - Removing the last remnants of Dragonfly!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622071723) do
+ActiveRecord::Schema.define(version: 20170626112643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -183,7 +183,6 @@ ActiveRecord::Schema.define(version: 20170622071723) do
 
   create_table "uploads", force: :cascade do |t|
     t.string   "file_name"
-    t.string   "file_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "pdf"
@@ -208,7 +207,6 @@ ActiveRecord::Schema.define(version: 20170622071723) do
     t.integer  "role"
     t.string   "title_en"
     t.integer  "locale"
-    t.string   "profile_image_uid"
     t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
