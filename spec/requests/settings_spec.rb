fdaf36e1ec2bd8caa2f2376e92982e0e0a585eea
David Wessman <david.wesmn@gmail.com>
2017-07-14 20:27:54 +0200

    Front page: allow embed video
    - Adds setting

---
+++ spec/requests/settings_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe('Settings', type: :request) do
+  let(:admin) { create(:user, :admin) }
+
+  it('visit settings and updates') do
+    sign_in(admin)
+    get(settings_path)
+    expect(response).to have_http_status(200)
+    attributes = {
+      settings: {
+        cover_image_ids: %w[1 3 3 7],
+        front_page_video: 'https://youtube.com'
+      }
+    }
+    post(settings_path, params: attributes)
+
+    expect(response).to redirect_to(settings_path)
+    follow_redirect!
+    expect(response).to have_http_status(200)
+    expect(Settings.front_page_video).to eq('https://youtube.com')
+    expect(Settings.cover_image_ids).to eq([1, 3, 7])
+  end
+end
